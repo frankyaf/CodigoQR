29f590939b1d0747af4f213b1ca42c4cedd2f0b9
ranky <ar.alonso.fuentes@gmail.com>
2022-03-09 20:41:21 -0600

    Qr

---
+++ creation.php
@@ -0,0 +1,17 @@
+<?php
+    include 'phpqrcode/qrlib.php';
+	
+    $contenido = "Armando Alonso Fuentes";
+
+    $filename = "qr.png";
+
+    $level = 'M';	
+    $tamanio = 10;
+    $frameSize = 3;
+    
+
+    Qrcode::png($contenido, $filename, $level, $tamanio, $frameSize);
+
+    echo "<div><img src='".$filename."'></div>";
+    
+?>
