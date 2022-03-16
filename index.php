88db1bc1f6923aa4b349668f9d9d32c02522bba0
ranky <ar.alonso.fuentes@gmail.com>
2022-03-15 21:14:29 -0600

    Edición de QR

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <main class= "contenedor"> 
+    <main class= "contenedor">  
     <header>
             <div class="portada">
                 <!-- <img src="imagenes/escudo_fes.png" alt="Fes Acatlán"> -->
@@ -30,33 +30,48 @@
                     <li><a href="#">Asistencia</a></li>
                 </ul>
     </nav>
-    <section class= "formulario">
+    <section class= "ventana">
         <div class="contenido">
             <div class="izquierda">
-                <h1>Formulario para la creación de códigos QR</h1>
-                <form method="get">
-                        <label for="Info">Texto</label>
-                        <input type="text" name="texto">
-                        <label for="nombre">Nombre del archivo</label>
-                        <input type="text" name="archive_name">
-                        <input type="submit" value="Enviar">
-                </form>
+                
+                    <h1>Formulario para la creación de códigos QR</h1>
+                    <form method="get">
+                            <label for="Info">Texto</label>
+                            <input type="text" name="texto">
+                            <br> <br>
+                            <label for="nombre">Nombre del archivo</label>
+                            <input type="text" name="archive_name">
+                            <br> <br>
+                            <label for="nivel">Nivel: </label>
+                            <select name="level" id="level">
+                                <option value="L">L</option>
+                                <option value="M">M</option>
+                                <option value="Q">Q</option>
+                                <option value="H">H</option>
+                            </select>
+                            <br> <br>
+                            <label for="size">Tamaño:</label>
+                            <input type="number" class="numb" name="numero" id="id_size" min="5" max="15" value= "5">
+                            <input type="submit" value="Enviar">
+                    </form> 
             </div>
 
             
             <div class="derecha">
-                <h3>Vista previa</h3>
+                <h3>Vista previa del código QR</h3>
                 <?php
                     
                     $contenido = $_GET['texto'];
                     $name = $_GET['archive_name'];
+                    $level = $_GET['level'];
+                    $tamanio = $_GET['numero'];
                     if(!empty($name)){
                         $filename = "temp/".$name.".png";
                     }
                     
-                    $level = 'M';	
-                    $tamanio = 10;
-                    $frameSize = 3;
+                    //$level = 'M';	
+                    //$tamanio = 5;
+                    $frameSize = 2;
                     
                     Qrcode::png($contenido, $filename, $level, $tamanio, $frameSize);
 
@@ -64,8 +79,12 @@
                 ?>
             </div>
         </div>  
-    </section>
+        </section>
 
+        <section class="foot">
+
+
+        </section>
     </main>
 </body>
 </html>
